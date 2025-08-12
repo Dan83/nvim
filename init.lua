f9faf393c65ba900cb88ed1833d4e7d1379c62c4
dan <dan83h@live.it>
2025-08-12 15:45:33 +0200

    Rust version

--- init.lua
+++ init.lua
@@ -1,28 +1,42 @@
-require("core.options")
-require("core.keymaps")
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
+local lazy_config = require "configs.lazy"
+
+-- load plugins
 require("lazy").setup({
-	require("plugins.neotree"),
-	require("plugins.colortheme"),
-	require("plugins.bufferline"),
-	require("plugins.lualine"),
-	require("plugins.treesitter"),
-	require("plugins.telescope"),
-	require("plugins.lsp"),
-	require("plugins.autocompletation"),
-	require("plugins.autoformatting"),
-	require("plugins.gitsigns"),
-	require("plugins.indent-blankline"),
-	require("plugins.misc"),
-	require("plugins.comment"),
-})
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "autocmds"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+
